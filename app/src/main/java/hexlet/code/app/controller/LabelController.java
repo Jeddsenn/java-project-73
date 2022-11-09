39dd77582c4a2c28a88b0acf4d994454289ed24d
vmj <theecrapdump@gmail.com>
2022-11-09 23:35:32 +0100

    fix PathVariable bug

--- app/src/main/java/hexlet/code/app/controller/LabelController.java
+++ app/src/main/java/hexlet/code/app/controller/LabelController.java
@@ -6,21 +6,32 @@ import hexlet.code.app.model.Label;
 import hexlet.code.app.repository.LabelRepository;
 import hexlet.code.app.service.LabelService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 import static hexlet.code.app.controller.LabelController.LABEL_CONTROLLER_PATH;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("${base-url}" + LABEL_CONTROLLER_PATH)
 public class LabelController {
     public static final String LABEL_CONTROLLER_PATH = "/labels";
-    private static final String ID = "/id";
+    private static final String ID = "/{id}";
 
-    LabelRepository labelRepository;
-    LabelService labelService;
+
+    private final LabelRepository labelRepository;
+
+    private final LabelService labelService;
 
 
     @GetMapping(ID)
@@ -33,6 +44,8 @@ public class LabelController {
         return labelRepository.findAll();
     }
 
+
+    @ResponseStatus(CREATED)
     @PostMapping("")
     public Label createLabel(@RequestBody LabelDto labelDto){
         return labelService.createLabel(labelDto);
