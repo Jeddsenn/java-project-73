f2fb471e0d91a62919eb935b26d53f0aed717486
vmj <theecrapdump@gmail.com>
2022-11-09 17:28:02 +0100

    add filtration

--- app/src/main/java/hexlet/code/app/controller/TaskController.java
+++ app/src/main/java/hexlet/code/app/controller/TaskController.java
@@ -1,15 +1,24 @@
 package hexlet.code.app.controller;
 
 
+import com.querydsl.core.types.Predicate;
 import hexlet.code.app.dto.TaskDto;
 import hexlet.code.app.model.Task;
 import hexlet.code.app.repository.TaskRepository;
 import hexlet.code.app.service.TaskService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.data.querydsl.binding.QuerydslPredicate;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 import static hexlet.code.app.controller.TaskController.TASK_CONTROLLER_PATH;
 import static org.springframework.http.HttpStatus.CREATED;
 
@@ -30,8 +39,8 @@ public class TaskController {
     }
 
     @GetMapping("")
-    public List<Task> getAllTasks(){
-        return taskRepository.findAll();
+    public List<Task> getAllTasks(@QuerydslPredicate final Predicate predicate) {
+        return predicate == null ? taskRepository.findAll() : taskRepository.findAll(predicate);
     }
 
     @PostMapping("")
