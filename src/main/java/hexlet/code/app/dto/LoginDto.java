28250a743959dc9e430175e43ff0648181a9a82f
vmj <theecrapdump@gmail.com>
2022-11-11 23:19:04 +0100

    refactor structure

---
+++ src/main/java/hexlet/code/app/dto/LoginDto.java
@@ -0,0 +1,32 @@
+package hexlet.code.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+
+    private String firstName;
+
+    private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String password;
+
+}
---
+++ src/main/java/hexlet/code/app/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package hexlet.code.app.repository;
+
+import hexlet.code.app.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    @Override
+    List<User> findAll();
+
+
+}
