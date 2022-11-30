b0de842e924fb55095eaa40e0249396a7cd1d920
vmj <theecrapdump@gmail.com>
2022-11-30 18:58:08 +0100

    fix

--- src/main/java/hexlet/code/config/RollbarConfig.java
+++ src/main/java/hexlet/code/config/RollbarConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration()
-@EnableWebMvc
 @ComponentScan({"hexlet.code"})
 public class RollbarConfig {
 
